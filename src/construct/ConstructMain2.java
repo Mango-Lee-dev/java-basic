a118818583a0e6730587f117635d7afa9c9f981d
wootaiklee <fined000@naver.com>
2025-08-15 18:17:55 +0900

    feat: Call Contruct method using this()

--- src/construct/ConstructMain2.java
+++ src/construct/ConstructMain2.java
@@ -1,11 +1,11 @@
 package construct;
 
-public class ContructMain1 {
+public class ConstructMain2 {
     public static void main(String[] args) {
-        MemberConstruct member1 = new MemberConstruct("user1", 15, 90);
-        MemberConstruct member2 = new MemberConstruct("user2", 16, 80);
+        MemberConstruct member1 = new MemberConstruct("User1", 15, 90);
+        MemberConstruct member2 = new MemberConstruct("User1", 15);
 
-        MemberConstruct[] members = { member1, member2 };
+        MemberConstruct[] members = {member1, member2};
 
         for (MemberConstruct member : members) {
             System.out.println("이름: " + member.name + " 나이: " + member.age + " 나이: " + member.grade);
