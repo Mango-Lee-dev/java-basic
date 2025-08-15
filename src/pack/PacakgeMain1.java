71918821cf8c168ba259a2f7e95398a5b4287408
wootaiklee <fined000@naver.com>
2025-08-15 18:55:27 +0900

    feat: package config 1

---
+++ src/pack/PacakgeMain1.java
@@ -0,0 +1,7 @@
+package pack;
+
+public class PacakgeMain1 {
+    public static void main(String[] args) {
+        Data data = new Data();
+    }
+}
---
+++ src/pack/a/User.java
@@ -0,0 +1,7 @@
+package pack.a;
+
+public class User {
+    public static void main(String[] args) {
+        System.out.println("Package pack.a");
+    }
+}
