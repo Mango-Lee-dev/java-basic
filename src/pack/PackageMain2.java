3bfa1421a59891e39f314afd925e2324333b9ba4
wootaiklee <fined000@naver.com>
2025-08-16 17:33:17 +0900

    Feat: Package * import

--- src/pack/PackageMain2.java
+++ src/pack/PackageMain2.java
@@ -1,7 +1,10 @@
 package pack;
 
-public class PacakgeMain1 {
+import pack.a.User;
+
+public class PackageMain2 {
     public static void main(String[] args) {
         Data data = new Data();
+        User user = new User();
     }
 }
