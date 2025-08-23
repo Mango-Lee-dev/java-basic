c7b0a8da1519fc1b6036aba41a5209239301984c
wootaiklee <fined000@naver.com>
2025-08-23 20:02:33 +0900

    feat: 자바 상속1

---
+++ src/ex2/CarMain.java
@@ -0,0 +1,9 @@
+package ex2;
+
+public class CarMain {
+    public static void main(String[] args) {
+        ElectricCar car = new ElectricCar();
+        car.charge();
+        car.move();
+    }
+}
---
+++ src/ex2/GasCar.java
@@ -0,0 +1,8 @@
+package ex2;
+
+public class GasCar extends Car {
+
+    public void fillup() {
+        System.out.println("gas를 충전합니다.");
+    }
+}
